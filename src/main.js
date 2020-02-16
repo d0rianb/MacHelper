139589eccaa10e7d1f37e6a51d806ddcc6a974be
d0rian <dorian.beauchesne@free.fr>
2020-02-16 17:44:50 +0100

    change path

--- src/main.js
+++ src/main.js
@@ -68,7 +68,7 @@ let app = new Vue({
     methods: {
         generateArticles: async function() {
             let articles = []
-            await fetch('./MacHelper/ressources/articles/shortcuts.json')
+            await fetch('../ressources/articles/shortcuts.json')
                 .then(data => data.json())
                 .then(json => {
                     for (let app in json) {
